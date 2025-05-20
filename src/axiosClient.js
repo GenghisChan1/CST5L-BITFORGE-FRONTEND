2263e37ff01890c0284b8204cf2c57376bd319d4
Lex <74861308+GenghisChan1@users.noreply.github.com>
2025-05-20 17:12:47 +0800

    update axios

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 const axiosClient = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   withCredentials: true,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  }
 });
 
 axiosClient.interceptors.request.use((config) => {
